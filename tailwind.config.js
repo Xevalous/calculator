810d4fdce9e2ae7e62d21dc5702716b6c1b97303
VEXG <helmiaryanto54@gmail.com>
2021-10-22 22:37:01 +0700

    -

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         fontFamily: {
-            poppins: '"Poppins", sans-serif;',
+            poppins: '"Poppins", sans-serif',
         },
         extend: {
             colors: {
