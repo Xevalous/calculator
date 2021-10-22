7dc8fa1ba5a054e4f2d9e47b5ef7d01501dcf4cf
VEXG <helmiaryanto54@gmail.com>
2021-10-22 20:21:18 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+let get_output = document.getElementById('output');
+
+function clear_() {
+    get_output.value = '';
+}
+
+function display(num = String) {
+    get_output.value += num;
+}
+
+function evaluate_() {
+    let input = get_output.value.replace(/÷/g, '/').replace(/×/g, '*').replace(/%/g, '/100');
+    try {
+        let eval_ = eval(input);
+        if (eval_) get_output.value = eval_;
+    } catch {
+        get_output.value = 'ERR';
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            poppins: '"Poppins", sans-serif;',
+        },
+        extend: {
+            colors: {
+                c1: '#212121',
+                c2: '#6d9886',
+                c3: '#d9cab3',
+                c4: '#f6f6f6',
+            },
+            transitionDelay: {
+                c25: '25ms',
+            },
+            borderRadius: {
+                'c-29': '.29rem',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
